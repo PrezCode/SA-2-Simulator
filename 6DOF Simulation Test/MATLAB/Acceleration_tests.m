5772057cc118c0922eeec3e7d5a3523de8f17ed8
Prez <153138580+PrezCode@users.noreply.github.com>
2026-01-30 19:34:16 -0600

    Delete 6DOF Simulation Test/MATLAB/Acceleration_tests.m

--- 6DOF Simulation Test/MATLAB/Acceleration_tests.m
+++
@@ -1,9 +0,0 @@
-% Acceleration as a function of mass
-
-time = [0 3];
-mass = @(time) 2397.9 - 174*time;
-force = @(time) 460000 - 130000*time;
-acceleration = @(time) (460000 - 130000*time)/(2397.9 - 174*time);
-
-fplot(time, acceleration);
-grid on;
